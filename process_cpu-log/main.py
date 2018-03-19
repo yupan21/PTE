c25c52ce1ef53b31fccb38484ae09ebc30c1db9f
dyllanwli <lidiyaa03@gmail.com>
2018-03-19 18:23:17 +0800

    upload fab

--- process_cpu-log/main.py
+++ process_cpu-log/main.py
@@ -4,22 +4,22 @@ import os
 from datetime import datetime
 
 
-
-
-def __init__(tStart,tEnd):
-    print("initializing...")
+def __initTime(tStart, tEnd):
+    # print("initializing...")
     tStart = datetime.fromtimestamp(tStart/1000).strftime("%I:%M:%S")
     tEnd = datetime.fromtimestamp(tEnd/1000).strftime("%I:%M:%S")
     print("tStart:", tStart)
     print("tEnd:", tEnd)
-    print("Command: sar -s {} -e {} > out_{}-{}.txt 2>&1".format(tStart,tEnd,tStart,tEnd))
-    return tStart,tEnd
+    # print("Command: sar -s {} -e {} > out_{}-{}.txt 2>&1".format(tStart, tEnd, tStart, tEnd))
+    return tStart, tEnd
 
-def readFile(fileName,tStart,tEnd):
-    # read txt
+
+def readFile(fileName, tStart, elasep):
+    # read cpu txt
     user_cpu = []
-
-    with open("./process_cpu-log/{}".format(fileName),"r") as file:
+    print("Loading", fileName,"-----")
+    pwd = os.getcwd()
+    with open("{}/process_cpu-log/{}".format(pwd, fileName), "r") as file:
         elasep_count = 0
         for i in file.readlines():
             i = i.strip()
@@ -31,16 +31,52 @@ def readFile(fileName,tStart,tEnd):
                     elasep_count += 1
                     if elasep_count == elasep:
                         print("The max cpu usage is:", max(user_cpu))
-                        print("The avg cpu usage is:", statistics.mean(user_cpu))
-                        return 
+                        print("The avg cpu usage is:",
+                              statistics.mean(user_cpu))
+                        return
             # break
+    return
 
-# You should modify you own data
-fileName = "pte_test_3_19_153.txt"
-tStart = 1521431255614
-tEnd = 1521431337889
-# run code
-elasep = int((tEnd-tStart)/1000)
-tStart,tEnd =  __init__(tStart,tEnd)
-print("Elasep time:", elasep)
-readFile(fileName,tStart,elasep)
+
+def readLog(fileName):
+    print("Loading...", fileName,"-----")
+    pwd = os.getcwd()
+    tag = True  # This tag is used to check time
+    with open("{}/CITest/Logs/{}".format(pwd, fileName), "r") as file:
+        # summary = []
+        for i in reversed(file.readlines()):
+            i = i.strip()
+            if i != "" and i.find("Test Summary") > -1:
+                if i.find("Test Summary:Total") > -1:
+                    print(i[10:])
+                if i.find("start") > -1 and tag == True and i.find("invoke_query_simple") == -1:
+                    tag = False
+                    index_1 = i.find("start")
+                    index_2 = i.find("end")
+                    # index_3 = i.find(",Throughput")
+                    index_3 = i.find(", #event")
+                    tStart = i[index_1+5:index_2].replace(" ", "")
+                    tEnd = i[index_2+3:index_3].replace(" ", "")
+                # summary.append(i)
+    return int(tStart), int(tEnd)
+
+
+def __main__():
+    # You should modify you own data
+    fileName_1 = "pte_test_3_19_153.txt"
+    fileName_2 = "pte_test_3_19_151.txt"
+    LogfileName = "RMT-3808-2i_0319172226.log"
+    tStart, tEnd = readLog(LogfileName)
+    # print(summary)
+    # tStart = 1521451381010
+    # tEnd = 1521448856460
+    # run code
+    print("-------------------")
+    elasep = int((tEnd-tStart)/1000)
+    tStart, tEnd = __initTime(tStart, tEnd)
+    print("Elasep time:", elasep)
+    readFile(fileName_1, tStart, elasep)
+    readFile(fileName_2, tStart, elasep)
+
+
+__main__()
