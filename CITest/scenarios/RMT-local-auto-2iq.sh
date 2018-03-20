0734f3b28f058d9210a6b0a3abfc43485ea48be8
dyllanwli <lidiyaa03@gmail.com>
2018-03-20 19:05:52 +0800

    remove duplicate cd

--- CITest/scenarios/RMT-local-auto-2iq.sh
+++ CITest/scenarios/RMT-local-auto-2iq.sh
@@ -13,7 +13,7 @@ node config.js nProcPerOrg 1
 # start network
 cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scripts
 echo "connecting to client host ans startup the network "
-ssh root@172.16.50.153 -i ~/.ssh/id_rsa "cd cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
+ssh root@172.16.50.153 -i ~/.ssh/id_rsa "cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
     bash RMT-network-auto.sh"
 bash ./test_driver.sh -t RMT-auto
 node config.js invokeType Query
