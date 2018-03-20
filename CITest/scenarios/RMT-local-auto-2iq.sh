1dbdc1bb7a757b05c9dc1380d422f2d6041db462
dyllanwli <lidiyaa03@gmail.com>
2018-03-20 18:58:36 +0800

    add auto

--- CITest/scenarios/RMT-local-auto-2iq.sh
+++ CITest/scenarios/RMT-local-auto-2iq.sh
@@ -6,8 +6,8 @@ bash /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/pro
 
 ## running a case 
 cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/RMT-auto/samplecc
-node config.js invokeType MOVE
-node config.js nProcPerOrg 5
+node config.js invokeType Move
+node config.js nProcPerOrg 1
 ## nProc has changed to 5
 
 # start network
@@ -16,8 +16,8 @@ echo "connecting to client host ans startup the network "
 ssh root@172.16.50.153 -i ~/.ssh/id_rsa "cd cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
     bash RMT-network-auto.sh"
 bash ./test_driver.sh -t RMT-auto
-node config.js invokeType QUERY
-node config.js nProcPerOrg 5
+node config.js invokeType Query
+node config.js nProcPerOrg 1
 bash ./test_driver.sh -t RMT-auto
 ## ending a case 
 
