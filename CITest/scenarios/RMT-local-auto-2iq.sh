c74e1309ab9fb415c595596de666f455248c1d11
dyllanwli <lidiyaa03@gmail.com>
2018-03-21 10:55:18 +0800

    reconfig

--- CITest/scenarios/RMT-local-auto-2iq.sh
+++ CITest/scenarios/RMT-local-auto-2iq.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# loading cpu moniting
+loading cpu moniting
 chmod +x /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log/manage_host.sh
 bash /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log/manage_host.sh
 
-# # start network
-# echo "connecting to client host ans startup the network "
-# ssh root@172.16.50.153 -i ~/.ssh/id_rsa "cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
-#     bash RMT-network-auto.sh"
-# echo "sleeping 6 sec"
-# sleep 6
+# start network
+echo "connecting to client host ans startup the network "
+ssh root@172.16.50.153 -i ~/.ssh/id_rsa "cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
+    bash RMT-network-auto.sh"
+echo "sleeping 6 sec"
+sleep 6
 
 
 # running test-----------------
@@ -39,3 +39,6 @@ bash ./test_driver.sh -t RMT-auto
 chmod +x /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log/calculate.sh
 cd /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log
 bash /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log/calculate.sh
+
+cd /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log
+python client-main.py /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/Logs
