4ccd4ffeb2e457bb7d4ada0f3366dcee69bd8bf7
dyllanwli <lidiyaa03@gmail.com>
2018-04-04 10:31:26 +0800

    add multiple test

--- process_cpu-log/manage_host.sh
+++ process_cpu-log/manage_host.sh
@@ -15,7 +15,7 @@ chmod +x ./record_system_stats.sh
 echo "running screen on local to record cpu usage..."
 screen -dmS local ./record_system_stats.sh
 
-echo "connecting to remote host to record cpu usage..."
+echo "connecting to remote ${HOST} to record cpu usage..."
 ssh root@$HOST -i ~/.ssh/id_rsa "cd /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/process_cpu-log/; \
     echo \"running screen on remote host...\"; \
     rm -rf *.txt ; \
