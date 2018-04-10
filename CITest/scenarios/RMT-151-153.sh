b15d6c04f227a88b8c84cd137cdb212c7b8249b2
dyllanwli <lidiyaa03@gmail.com>
2018-04-10 11:18:48 +0800

    local machine test

--- CITest/scenarios/RMT-151-153.sh
+++ CITest/scenarios/RMT-151-153.sh
@@ -13,6 +13,8 @@ CISCRIPT_DIR=$GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test
 # use to handel the whole test driver and test case config
 NL_DIR=/opt/go/src/github.com/hyperledger/fabric-test/tools/NL
 # launch you network in this directory
+SCFILES_DIR=/opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/CISCFiles
+# scfile needs for PTE test
 
 echo "Make sure your host is running on swarm mode."
 echo "Make sure your character is manager."
@@ -23,22 +25,27 @@ echo "Make sure your network yaml file have beed prepared."
 # config scfiles -------------
 echo "Configing PTE SCfiles"
 cd $CISCRIPT_DIR 
-node ./config_sc.js RMT-config-multi.json orderer.orderer0.url grpcs://${HOST1}:5005
-node ./config_sc.js RMT-config-multi.json orderer.orderer1.url grpcs://${HOST1}:5005
-node ./config_sc.js RMT-config-multi.json orderer.orderer2.url grpcs://${HOST1}:5005
+node ./config_sc.js RMT-config-multi.json orderer.orderer0.url grpcs://$HOST1:5005
+node ./config_sc.js RMT-config-multi.json orderer.orderer1.url grpcs://$HOST1:5006
+node ./config_sc.js RMT-config-multi.json orderer.orderer2.url grpcs://$HOST1:5007
 
-node ./config_sc.js RMT-config-multi.json org1.ca.url https://${HOST2}:7054
-node ./config_sc.js RMT-config-multi.json org1.peer1.requests grpcs://{HOST2}}:7061
-node ./config_sc.js RMT-config-multi.json org1.peer1.events grpcs://{HOST2}}:6051
-node ./config_sc.js RMT-config-multi.json org1.peer2.requests grpcs://{HOST2}}:7062
-node ./config_sc.js RMT-config-multi.json org1.peer2.events grpcs://{HOST2}}:6052
+node ./config_sc.js RMT-config-multi.json org1.ca.url https://$HOST2:7054
+node ./config_sc.js RMT-config-multi.json org1.peer1.requests grpcs://$HOST2:7061
+node ./config_sc.js RMT-config-multi.json org1.peer1.events grpcs://$HOST2:6051
+node ./config_sc.js RMT-config-multi.json org1.peer2.requests grpcs://$HOST2:7062
+node ./config_sc.js RMT-config-multi.json org1.peer2.events grpcs://$HOST2:6052
 
-node ./config_sc.js RMT-config-multi.json org2.ca.url https://${HOST2}:7055
-node ./config_sc.js RMT-config-multi.json org2.peer1.requests grpcs://{HOST2}}:7063
-node ./config_sc.js RMT-config-multi.json org2.peer2.requests grpcs://{HOST2}}:6053
-node ./config_sc.js RMT-config-multi.json org2.peer1.events grpcs://{HOST2}}:7064
-node ./config_sc.js RMT-config-multi.json org2.peer2.events grpcs://{HOST2}}:6054
+node ./config_sc.js RMT-config-multi.json org2.ca.url https://$HOST2:7055
+node ./config_sc.js RMT-config-multi.json org2.peer1.requests grpcs://$HOST2:7063
+node ./config_sc.js RMT-config-multi.json org2.peer1.events grpcs://$HOST2:6053
+node ./config_sc.js RMT-config-multi.json org2.peer2.requests grpcs://$HOST2:7064
+node ./config_sc.js RMT-config-multi.json org2.peer2.events grpcs://$HOST2:6054
+
+echo "Sending scfile to $HOST2"
+cd $SCFILES_DIR
+scp -i ~/.ssh/id_rsa ./RMT-config-multi.json root@$HOST2:$SCFILES_DIR
 # config scfiles -------------
+
 
 # cleanup the network ------------
 for HOST in $HOST1 $HOST2; do
--- CITest/scripts/test_chaincode.sh
+++ CITest/scripts/test_chaincode.sh
@@ -5,7 +5,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+GOPATH=/opt/go
 FabricTestDir=$GOPATH/src/github.com/hyperledger/fabric-test
 SDKDir=$FabricTestDir/fabric-sdk-node
 
