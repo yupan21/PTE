4e12808a74e1c59d4f5732622cc541ac9e2763a8
dyllanwli <lidiyaa03@gmail.com>
2018-04-11 11:37:17 +0800

    starting add some feature about PTE

--- CITest/scenarios/RMT-ecs-single.sh
+++ CITest/scenarios/RMT-ecs-single.sh
@@ -21,15 +21,15 @@ LOGS_DIR=/opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE
 
 
 
-# start network ---
-echo "connecting host and startup the network "
-ssh root@${HOST} -i ~/.ssh/id_rsa "cd $CISCRIPT_DIR; \
-    echo \"changing network ...\"; \
-    python config-network.py RMT-ecs -o 3 -x 2 -r 2 -p 2 -n 1 -t solo -f test -w 0.0.0.0 -S enabled -c 2s -l INFO -B 2000 ;\
-    chmod +x ./test_nl.sh; \
-    cd /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
-    bash RMT-network-ecs2.sh ${LOCAL}"
-## ending case ---
+# # start network ---
+# echo "connecting host and startup the network "
+# ssh root@${HOST} -i ~/.ssh/id_rsa "cd $CISCRIPT_DIR; \
+#     echo \"changing network ...\"; \
+#     python config-network.py RMT-ecs -o 3 -x 2 -r 2 -p 2 -k 1 -z 1 -n 1 -t kafka -f test -w 0.0.0.0 -S enabled -c 2s -l INFO -B 2000 ;\
+#     chmod +x ./test_nl.sh; \
+#     cd /opt/go/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/PTE/CITest/scenarios/network; \
+#     bash RMT-network-ecs2.sh ${LOCAL}"
+# ## ending case ---
 
 
 # loading moniting
@@ -43,6 +43,8 @@ cd $CISCRIPT_DIR
 node ./config.js RMT-ecs nProcPerOrg 1
 node ./config.js RMT-ecs nRequest 10
 node ./config.js RMT-ecs runDur 0
+node ./config.js RMT-ecs ccOpt.payLoadMin 256
+node ./config.js RMT-ecs ccOpt.payLoadMax 256
 node ./config.js RMT-ecs invokeType Move
 bash ./test_driver.sh -t RMT-ecs
 ## ending case ----------------
